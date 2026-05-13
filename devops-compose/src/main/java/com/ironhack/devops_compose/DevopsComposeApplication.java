c93d8fbe3921ac4b20997e70be11666e5a6e253d
TheGlitch26 <nihad.bagirzade888@gmail.com>
2026-05-13 12:15:03 +0400

    Docker configs are ready (i guess)

--- devops-compose/src/main/java/com/ironhack/devops_compose/DevopsComposeApplication.java
+++ devops-compose/src/main/java/com/ironhack/devops_compose/DevopsComposeApplication.java
@@ -8,6 +8,7 @@ public class DevopsComposeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsComposeApplication.class, args);
+		System.out.println("Application runs successfully!");
 	}
 
 }
