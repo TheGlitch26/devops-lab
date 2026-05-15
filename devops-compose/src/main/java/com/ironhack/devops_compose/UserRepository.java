1fee4ca11e6800c0adc49f80c09302b754f2f525
TheGlitch26 <nihad.bagirzade888@gmail.com>
2026-05-15 13:25:00 +0400

    fix: fixed general folder structure

--- devops-compose/src/main/java/com/ironhack/devops_compose/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ironhack.devops_compose;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
